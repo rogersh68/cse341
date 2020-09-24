6b42ac4455d9bc1cd02a26399f511527fa8c987e
Hannah Rogers <rog19008@byui.edu>
2020-09-24 13:29:30 -0600

    adding css

--- web/index.php
+++ web/index.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hannah Rogers | Home</title>
+    <link rel="stylesheet" src="/styles.css">
 </head>
 <body>
+    <!-- Navigation -->
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/nav.php'; ?>
     <header>
-        <!-- Navigation -->
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/nav.php'; ?>
-        <image src="images/home.jpg" alt="banner">
+        <h1>Welcome</h1>
     </header>
     <main>
-        <h1>Welcome to Hannah Rogers' Homepage</h1>
-        <a href="/intro.php">Intro</a>
-        <a href="/assignments.php">Assignments</a>
+        <h2>Hannah Rogers - CSE 341<h2>
+        <a class="link" href="/intro.php">Intro</a>
+        <a class="link" href="/assignments.php">Assignments</a>
     </main>
     <footer>
         <?php echo date("F j, Y"); ?>
     </footer>
 </body>
 </html>
-
