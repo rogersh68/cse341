67b93e5317ed349bea1153559267e8c93c96e721
Hannah Rogers <rog19008@byui.edu>
2020-10-02 10:16:44 -0600

    testing items

--- web/prove03/items.php
+++ web/prove03/items.php
@@ -3,16 +3,18 @@ session_start();
 $items = array();
 $i = 0;
 
-if (isset($_POST['delete'])) {
+/*if (isset($_POST['delete'])) {
 	unset($_SESSION[$_POST['delete']]);
 	header('Location: cart.php');
-}
+}*/
 
-else {
+if (isset($_SESSION['items'])) {
+	$i = count($_SESSION['items']);
+}
 	foreach ($_POST as $key => $value) {
 		$items[$i] = $key;
 		$i++;
-	}
+	
 
 	$_SESSION['items'] = $items;
 	header('Location: index.php');
