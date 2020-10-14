eb7b1fb48a8aa10175000e8147962cf124ccc4fa
Hannah Rogers <rog19008@byui.edu>
2020-10-14 17:24:39 -0600

    testing css

--- web/prove05/commission.php
+++ web/prove05/commission.php
@@ -18,38 +18,42 @@ include 'common/connection.php';
     ?>
 
     <main>
-        <h1>Commission</h1>
-        <form action="account.php" method="post">
-            <label for="creator">Who would you like to commission?</label>
-            <?php 
-            //populate select list with creators
-            echo "<select name='creator' id='creatorList'>";
-            echo "<option>Select</option>";
-            foreach ($db->query('SELECT userid, firstname, lastname FROM public.user WHERE creator = TRUE') as $row) {
-                echo "<option value='".$row['userid']."'>".$row['firstname']." ".$row['lastname']."</option>";
-            }
-            echo "</select>"
-            ?>
+        <h1>Commissions</h1>
+        <div class="commissions_div">
+            <form action="account.php" method="post">
+                <label for="creator">Who would you like to commission?</label>
+                <?php 
+                //populate select list with creators
+                echo "<select name='creator' id='creatorList'>";
+                echo "<option>Select</option>";
+                foreach ($db->query('SELECT userid, firstname, lastname FROM public.user WHERE creator = TRUE') as $row) {
+                    echo "<option value='".$row['userid']."'>".$row['firstname']." ".$row['lastname']."</option>";
+                }
+                echo "</select>"
+                ?>
 
-            <label for="commDesc">What would you like to commission?</label>
-            <textarea name="commDesc"></textarea>
+                <label for="commDesc">What would you like to commission?</label>
+                <textarea name="commDesc"></textarea>
 
-            <p>Please note that whoever you choose must accept the commission.</p>
-            <input type="submit" value="Send Request">
-        </form>
+                <p>Please note that whoever you choose must accept the commission.</p>
+                <input type="submit" value="Send Request">
+            </form>
 
-        <h2>Get to know our creators</h2>
+            <div class="comm_creators_container">
+                <h2>Get to know our creators</h2>
 
-        <div class="creators_div">
-            <?php
-            foreach ($db->query('SELECT firstname, lastname, userimg, creatordesc FROM public.user WHERE creator = TRUE') as $row) {
-                echo "<div class='creator_desc'>";
-                echo "<img src='".$row['userimg']."' alt='creator'>";
-                echo "<h3>".$row['firstname']." ".$row['lastname']."</h3>";
-                echo "<p>".$row['creatordesc']."</p>";
-                echo "</div>";
-            }
-            ?>
+                <div class="creators_div">
+                    <?php
+                    foreach ($db->query('SELECT firstname, lastname, userimg, creatordesc FROM public.user WHERE creator = TRUE') as $row) {
+                        echo "<div class='creator_desc'>";
+                        echo "<img src='".$row['userimg']."' alt='creator'>";
+                        echo "<h3>".$row['firstname']." ".$row['lastname']."</h3>";
+                        echo "<p>".$row['creatordesc']."</p>";
+                        echo "</div>";
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
 
     </main>
