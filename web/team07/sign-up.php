717051aedc62e4207e40e56f91cf8495751a305b
Hannah Rogers <rog19008@byui.edu>
2020-10-26 13:32:39 -0600

    team activity

--- web/team07/sign-up.php
+++ web/team07/sign-up.php
@@ -27,12 +27,7 @@ include './prove06/common/connection.php';
         $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
         $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-        echo $username;
-        echo $password;
-
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-        echo $hashedPassword;
 
         try {
             $stmt = $db->prepare('INSERT INTO team_user (username, userpassword) VALUES (:username, :userpassword)');
